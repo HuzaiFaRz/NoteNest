11b98878dc5576325fc433ed54773a497fa9513c
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2025-02-08 23:20:05 +0500

    Port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3000;
+const PORT = 9999;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
