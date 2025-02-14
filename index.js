cf7f3db3353d95ef97a4458d640ccc8683646f86
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2025-02-14 14:02:25 +0500

    Notes Render with Ejs

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const PORT = 9999;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+fs.readdir(`./notes`, (error, notes) => {
+  if (error) {
+    return "error";
+  }
+  app.get("/", (req, res) => {
+    res.render("index", { notes: notes });
+  });
+  console.log(notes);
 });
+
+app.post('/create',(req,res)=>{
+
+
+  
+})
 
 app.listen(PORT, () => {
   console.log("Server Running");
